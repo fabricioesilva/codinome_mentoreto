c9fb9bdf7e5256390273e6dc37c1f6caea054c6a
FABRÍCIO ABREU E SILVA <fabreuesilva@mylenovo>
2023-07-28 18:38:39 -0300

    requirements alterado

--- core/settings.py
+++ core/settings.py
@@ -104,6 +104,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'expert_db',
+        'PASSWORD': 'etEn2@dmSU#$8',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
